31f411afeee9aa3aa3e414f0630586426fb90e8e
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-06-13 20:10:14 -0300

    melhoria de layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,8 @@
 
                         <div class="form-group row mb-2">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn btn-primary btn-sm text-center" style="heigth: 30px;">
+                                    {{ __('Entrar') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-2">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-sm text-center" style="heigth: 30px;">
                                     {{ __('Registrar') }}
                                 </button>
                             </div>
